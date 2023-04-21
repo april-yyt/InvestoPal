66952709a5ff8bf7170cbf628cb220491526df36
april-yyt <aprilytyang@gmail.com>
2023-04-20 18:14:49 -0700

    Initial Commit

---
+++ InvestoPal/InvestoPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestoPalApp.swift
+//  InvestoPal
+//
+//  Created by April Yang on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct InvestoPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
